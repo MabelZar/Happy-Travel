9796397c89f62521f4f75673424ab72388fe8795
Cris <crispr.zde@gmail.com>
2024-08-23 17:45:30 +0200

    database connection exception created

--- travel/src/main/java/com/travel/travel/exception/HappyTravelResponseEntityExceptionHandler.java
+++ travel/src/main/java/com/travel/travel/exception/HappyTravelResponseEntityExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.travel.travel.exception;
 
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -22,6 +23,16 @@ public class HappyTravelResponseEntityExceptionHandler extends ResponseEntityExc
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(bodyErrorMessage);
 
+    }
+
+    @ExceptionHandler(DataAccessException.class) // Error global que solo se indica aquí
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<BodyErrorMessage> manejarErrorDeConexion(DataAccessException ex) {
+        BodyErrorMessage response = new BodyErrorMessage();
+        response.setHttpStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        response.setMessage("Error de conexión con la base de datos: " + ex.getMessage()); //Por eso el mensaje se pone aquí
+        
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
