a409c479337cae04050354a03b1eb4c88432e3e2
paolaperdomo <paolaperdomo079@gmail.com>
2024-08-25 17:30:32 +0200

    first implementations of authentication

--- travel/src/main/java/com/travel/travel/repositories/UserRepository.java
+++ travel/src/main/java/com/travel/travel/repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package com.travel.travel.repositories;
 
+
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import com.travel.travel.models.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer>{
+    Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
 }
